3d06607ae3f96f923376826414fc47758c64f353
Jason Nastovski <jnastovski@protonmail.com>
2019-12-09 11:53:38 -0800

    generate ast for each svelte file

--- extension/devtools.js
+++ extension/devtools.js
@@ -1,10 +1,32 @@
+import svelte from 'svelte/compiler';
+
+// globals
+let i = 0;
+const componentNames = [];
+let unorderedListOfNodesForDependencyGraph = [];  // This is a pre- or partial tree with known relationships among componenets/files that go only 1 layer deep (this is all we need to build the rest of the tree)
+let componentTree;
+
 chrome.devtools.panels.create(
   'Svelte Tool',
   null,
-  'panel.html',
-  panel => {
-    chrome.devtools.inspectedWindow.getResources((resources) => {
+  'devtools.html',
+  (panel) => {
+    panel.onShown.addListener(() =>
+      chrome.devtools.inspectedWindow.getResources((resources) => {
 
-    })
+        const arrSvelteFiles = resources.filter(file => file.url.includes('.svelte'));
+        
+        arrSvelteFiles.forEach((el) => componentNames.push(`<${el.url.split('').reverse().join('').slice(7, el.url.split('').reverse().join('').indexOf('/')).split('').reverse().join('')} />`))
+
+        arrSvelteFiles.map((resource) => {
+          resource.getContent((source) => {
+            if (source) {
+              const ast = svelte.parse(source);
+              console.log(ast);
+            }
+          })
+        })
+      })
+    )
   }
 )
