3b72f9a7e6b6ee3ceff2c6f930691dab554a6262
Jason Nastovski <jnastovski@protonmail.com>
2019-12-09 12:19:38 -0800

    build pre- or partial tree: an unordered array of tree nodes that are the names of svelte components with dependencies one level deep

--- extension/devtools.js
+++ extension/devtools.js
@@ -16,13 +16,37 @@ chrome.devtools.panels.create(
 
         const arrSvelteFiles = resources.filter(file => file.url.includes('.svelte'));
         
-        arrSvelteFiles.forEach((el) => componentNames.push(`<${el.url.split('').reverse().join('').slice(7, el.url.split('').reverse().join('').indexOf('/')).split('').reverse().join('')} />`))
+        arrSvelteFiles.forEach((el) => componentNames.push(`<${el.url.split('').reverse().join('').slice(7, el.url.split('').reverse().join('').indexOf('/')).split('').reverse().join('')} />`));
 
         arrSvelteFiles.map((resource) => {
           resource.getContent((source) => {
             if (source) {
+
               const ast = svelte.parse(source);
-              console.log(ast);
+
+
+              function createAsset() {
+                const obj = {};
+                const t1 = {};
+                const t2 = {};
+
+                ast.instance.content.body.forEach(function(el) {
+                  if (el.type === "ImportDeclaration" && el.source.value.indexOf('.svelte') !== -1) {
+                    t1[`<${el.source.value.slice(2, el.source.value.length - 7)} />`] = null;
+                  }
+                })
+                
+                if (Object.entries(t1).length !== 0) {
+                  obj[componentNames[i]] = t1;
+                } else {
+                  obj[componentNames[i]] = null;
+                }
+
+                if (Object.entries(obj).length !== 0) {
+                  unorderedListOfNodesForDependencyGraph.push(obj);
+                }
+              }
+              createAsset();
             }
           })
         })
