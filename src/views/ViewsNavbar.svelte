e14f0bca9b7e9eaaeba403c61650775c5cc2a2f8
unknown <sungdo1004@gmail.com>
2019-12-13 17:08:56 -0800

    added a button and chrome devtool functionality template for the button

--- src/views/ViewsNavbar.svelte
+++ src/views/ViewsNavbar.svelte
@@ -1,7 +1,35 @@
 <script>
+  import svelte from 'svelte/compiler'
+
+  //creates function fetchData that will be invoked as user clicks a button
+  const fetchData = () => {
+    // grabs all the associated files when devtool is open and then runs the callback
+    chrome.devtools.inspectedWindow.getResources((resources) => {
+      const arrSvelteFiles = resources.filter(file => file.url.includes('.svelte'));
+    
+    let AST = []
+    let urls = []
+
+    //retrieves each urls and pushes onto urls array
+    for (let i = 0; i < arrSvelteFiles.length; i++) {
+        urls.push(JSON.parse(JSON.stringify(arrSvelteFiles[i])))
+        arrSvelteFiles[i].getContent(content => {
+          AST.push(svelte.parse(content))
+        })
+     }
+
+    
+    // runs next logic after async svelte.parse is completed
+    setTimeout(()=> {
+
+    }, 100)
+    })
+
+  }
 </script>
 
 <div id="views-navbar">
+  <button on:click={fetchData}>Get Data</button>
 </div>
 
 <style>
