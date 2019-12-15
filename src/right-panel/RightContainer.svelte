c43e081ec2f9979774f3a416b08c6e77d319d9e4
Kenneth Trang <trang.kenneth@gmail.com>
2019-12-14 16:33:48 -0800

    Add right panel (app now split between left and right panels)

---
+++ src/right-panel/RightContainer.svelte
@@ -0,0 +1,16 @@
+<script>
+  import StatesContainer from './states/StatesContainer.svelte';
+  
+</script>
+
+<div id='right-container'>
+  <StatesContainer />
+</div>
+
+<style>
+  #right-container {
+    display: grid;
+    grid-template-rows: 70% 30%;
+  }
+  
+</style>
