72af7cb3f250fd3b595bc28eaab0b8eaea178c7c
Kenneth Trang <kennethtrang@Kenneths-MacBook-Pro-1322.local>
2019-12-18 19:50:17 -0800

    Establish messaging communication between content scripts and background page

--- extension/contentscript.js
+++ extension/contentscript.js
@@ -1,6 +1,3 @@
+chrome.runtime.sendMessage({ msg: 'hello from content' });
+
 console.log('hello from content.js');
-
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  console.log(msg.msg);
-
-})
