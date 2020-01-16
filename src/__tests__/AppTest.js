e6d9d7b1311b74635a2853ee5c9491ee54b15905
Kenneth Trang <trang.kenneth@gmail.com>
2020-01-16 14:39:02 -0800

    Add App tests

--- src/__tests__/AppTest.js
+++ src/__tests__/AppTest.js
@@ -1,35 +1,20 @@
 import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent } from '@testing-library/svelte';
 import App from '../App';
-import ViewsNavbar from '../left-panel/ViewsNavbar.svelte';
-import chrome from 'sinon-chrome';
 
 
-// test('left panel renders on load', () => {
-//   const leftPanel = document.querySelector('#views-container');
-//   const { results } = render(App);
 
-//   expect(results).toContainElement(leftPanel);
-// })
-describe('test.js', () => {
-  before(function () {
-    global.chrome = chrome;
-  }); 
+test('Should render left panel on load', () => {
+  const { results } = render(App);
+  const leftPanel = document.querySelector('#views-container');
 
-  test('changes button text on click', async () => {
-    const { getByText } = render(ViewsNavbar)
-    const button = getByText('Tree')
-  
-    // Using await when firing events is unique to the svelte testing library because
-    // we have to wait for the next `tick` so that Svelte flushes all pending state changes.
-    await fireEvent.click(button)
-  
-    expect(button).toHaveTextContent('Tree')
-  })
+  expect(results).toContainElement(leftPanel);
+})
 
-  after(function () {
-    chrome.flush();
-    delete global.chrome;
-  });
+test('Should render right panel on load', () => {
+  const { results } = render(App);
+  const rightPanel = document.querySelector('#right-container');
+
+  expect(results).toContainElement(rightPanel);
 })
 
