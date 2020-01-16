d08bba55550fdbf86ea192130a56d6e9c0d55eec
Kenneth Trang <trang.kenneth@gmail.com>
2020-01-16 15:22:17 -0800

    Delete svelte.config

--- svelte.config.js
+++
@@ -1,8 +0,0 @@
-const sveltePreprocess = require('svelte-preprocess');
-
-module.exports = {
-  preprocess: sveltePreprocess({
-    // ...svelte-preprocess options
-  }),
-  // ...other svelte options
-};
